49aa42ec055540c337b188c4a0d6fdb90beef986
dingo <nicklmoignard@gmail.com>
2017-10-27 00:02:26 +1000

    player movement class complete

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -8,77 +8,70 @@ public class PlayerMovement : MonoBehaviour {
     private Vector3 moveVector;
     private float verticalVelocity;
     private int noJumps;
-    public int maxJumps = 2;
-    private BoxCollider feet;
-    private bool isJumping;
-    public Collider[] attackHitboxes;
-    public float moveSpeed = 5;
     private float lastJumpTime;
+    private bool isJumping;
+
+    public float moveSpeed = 5;
+    public float gravity = 14;
+    public int maxJumps = 2;
 
 
-	// Use this for initialization
-	void Start () {
+    void Start () {
         controller = GetComponent<CharacterController>();
-        feet = GetComponent<BoxCollider>();
-
-
-        Debug.Log("the logger works");
 	}
-	
-
-
 	
 	void FixedUpdate () {
         Gravity();
 
-        
-
+        // Create movement vector
         moveVector = Vector3.zero;
         moveVector.x = Input.GetAxis("Horizontal") * moveSpeed;
         moveVector.y = verticalVelocity;
-
+        moveVector.z =  -transform.position.z;  // keep the player locked to the z axis
         controller.Move(moveVector * Time.deltaTime);
 	}
 
     void Gravity()
     {
         // check jump timer and reset
-        Debug.Log(isJumping);
-
         if (lastJumpTime - Time.fixedTime < -0.2 )
         {
             isJumping = false;
         }
 
+
+        // constant gravity holding player to ground
         if (controller.isGrounded && !isJumping)
         {
             verticalVelocity = -1;
             
-
+        // apply falling gravity (terminal velocity)
         } else 
         {
-            verticalVelocity -= 14 * Time.deltaTime;
+            verticalVelocity -= gravity * Time.deltaTime;
         }
     }
 
     private void Jump()
     {
-
-        
-        if (controller.isGrounded)
+        if (controller.isGrounded)  // if player standing om ground
         {
-            noJumps = 0;
+            noJumps = 1; // first jump
             isJumping = true;
-            verticalVelocity = 10f;
+            verticalVelocity = 10f;  // apply jump force
             lastJumpTime = Time.fixedTime; // start timer
+
         } else if (noJumps <= maxJumps)
         {
+            noJumps = noJumps + 1; // add additional jump
             isJumping = true;
             verticalVelocity = 10f;
-            lastJumpTime = Time.fixedTime; // start timer
+            lastJumpTime = Time.fixedTime;
         }
     }
 
+
+    // Listen for Input Manager events
     private void OnEnable()
     {
         EventManager.StartListening("jump", Jump);
